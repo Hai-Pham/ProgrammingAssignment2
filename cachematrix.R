00d7c634fa69412121f1b0b16daf9ce66ea880d6
Hai-Pham <haipt.hut@gmail.com>
2015-01-22 06:54:57 -0600

    Complete file for Programming Assignment 2
    MooC class: R Programming @Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL #inversed matrix
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i 
+        
+        list(set=set, get=get, 
+             setinverse=setinverse, 
+             getinverse=getinverse)
 }
 
 
@@ -12,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("the inverse of same matrix is already cached")
+                message("result is")
+                return(i)
+        }
+        
+        data <- x$get() #no cache, load the matrix to compute
+        i <- solve(data,...) #do the inversion by solve()
+        x$setinverse(i) #assign i to x
+        i #return the inversed matrix
 }
